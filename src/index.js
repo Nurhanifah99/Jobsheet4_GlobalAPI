1b3f0d56f2e73dfadb4d292fe73711790e5d2859
Nurhanifah99 <Hanifah110498@gmail.com>
2021-04-10 20:38:39 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import BlogPost from './component/container/BlogPost/BlogPost1'///tugas praktikum ListArtikel
+import BlogPost from './component/container/BlogPost/BlogPost'//mahasiswa
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BlogPost />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('content')
 );
 
 // If you want to start measuring performance in your app, pass a function
